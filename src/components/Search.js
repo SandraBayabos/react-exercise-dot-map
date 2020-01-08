efb98bd66f307951906617c65d6997c9cfc6bbec
Sandra Bayabos <sandra.bayabos@gmail.com>
2020-01-08 18:51:03 +0800

    used useRef for input change

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,22 @@
 import React, { useRef } from "react";
 
 const Search = () => {
+  const input = useRef(null);
+
+  function filterUpdate() {
+    console.log(input.current.value);
+  }
+
   return (
     <div>
       <header>
         <form>
-          <input type="text" placeholder="Type to filter..."></input>
+          <input
+            type="text"
+            placeholder="Type to filter..."
+            ref={input}
+            onChange={filterUpdate}
+          ></input>
         </form>
       </header>
     </div>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import data from "./data/Data";
+import "normalize-css";
 
 // import data and pass it down as props
 ReactDOM.render(<App data={data} />, document.getElementById("root"));
